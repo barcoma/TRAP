2af2d8281df86c45de7e11cd2bb9474e1512a089
Robin-Sb <robin.schwab@hs-furtwangen.de>
2018-12-12 17:00:08 +0100

    updated config for pwa

---
+++ src/components/OBD.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: 'OBD',
+    mounted() {
+    this.$http.get('http://192.168.0.10:35000').then(response => {
+
+    // get body data
+    this.someData = response.body;
+    console.log(someData);
+
+  }, response => {
+    // error callback
+  });
+    }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,19 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   }
+  // , pwa: {
+  //   name: 'My App',
+  //   themeColor: '#4DBA87',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'black',
+
+  //   // configure the workbox plugin
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     // swSrc is required in InjectManifest mode.
+  //     swSrc: 'src/service-worker.js'
+  //     // ...other Workbox options...
+  //   }
+  // }
 }
