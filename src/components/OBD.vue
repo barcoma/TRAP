a8164b115c13c8f0a3efc92f29d79128da90fb86
Robin-Sb <robin.schwab@hs-furtwangen.de>
2018-12-17 20:02:38 +0100

    added basic offline functionality

---
+++ src/components/OBD.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <section class="OBD">
+        <button v-on:click="getDevices">Request devices</button>
+        
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: "OBD",
+    data: {
+
+    }, 
+    methods: {
+        getDevices: function() {
+            navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            optionalServices: ['battery_service']
+            })
+            .then(device => { /* ... */ })
+            .catch(error => { console.log(error); });
+        }
+    }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,19 @@ module.exports = {
   lintOnSave: false,
   configureWebpack: {
     devtool: 'source-map'
+  },
+  pwa: {
+    name: 'trap',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js'
+    }
   }
 }
