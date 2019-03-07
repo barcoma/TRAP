132ceb3d0d533a7bfb286d5205dac7f1a4cbf069
Robin <robin.schwab@hf-furtwangen.de>
2019-03-07 11:31:36 +0100

    minor tests

--- src/views/ObdPage.vue
+++ src/views/ObdPage.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+var serviceUUID = "e7810a71-73ae-499d-8c15-faa9aef0c3f2";
+var charUUID = "bef8d6c9-9c21-4c9e-b632-bd58c1009f9f";
 export default {
     name: "OBD",
     data: {
@@ -14,14 +16,38 @@ export default {
         getDevices: function() {
             navigator.bluetooth.requestDevice({
             acceptAllDevices: true,
-            optionalServices: ['battery_service']
+            optionalServices: [serviceUUID]
             })
             .then(device => { 
                 console.log(device.name);
                 return device.gatt.connect();
             })
-            .then(server => { /* ... */ })
-            .catch(error => { console.log(error); });
+            .then(server => { 
+                console.log(server);
+                return server.getPrimaryService(serviceUUID);
+            })
+            .then(service => {
+                console.log(service);
+                return service.getCharacteristic(charUUID);
+            })
+            .then(characteristic => {
+                console.log(characteristic);
+                let encoder = new TextEncoder('utf-8');
+                let message = encoder.encode("ATZ\r\n");
+                var data = new Uint8Array([0x01, 0x00, 0x0D]);
+
+                // var x = '\x01\x00\r'
+                // const buffer = new ArrayBuffer(3);
+                // const uint8 = new Uint8Array(buffer);
+                // uint8.set([01, 00, 13]);
+
+                characteristic.writeValue(message);
+                return characteristic.readValue();
+            })
+            .then(value => {
+                console.log(value);
+            })
+            .catch(error => { console.log(error.message); });
         }
     
     },
