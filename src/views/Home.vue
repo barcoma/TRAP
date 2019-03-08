7acf3429b73ca340413b443ef285733a6a463808
Henri Joß <henricharles@web.de>
2019-03-08 11:53:10 +0100

    Carousel for Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <v-container fluid>
-    <v-layout row mb-2>
+    <!-- <v-layout row mb-2>
 
       <v-flex xs12>
-        <v-card dark tile flat color="error">
+        <v-card dark tile flat color="blue">
           
             <v-layout row>
               <v-flex xs6>
-                <v-card dark tile flat color="red darken-4">
+                <v-card dark tile flat color="blue">
                   <v-card-text>Willkommen</v-card-text>
                 </v-card>
               </v-flex>
               <v-flex xs5 offset-xs1>
-                <v-card dark tile flat color="red darken-4">
+                <v-card dark tile flat color="blue">
                   <v-card-text>{{ time }}<br>{{ date }}</v-card-text>
                 </v-card>
               </v-flex>
@@ -27,24 +27,61 @@
     <v-layout row>
 
         <v-flex xs6 mr-1>
-          <v-card dark tile flat color="red darken-4">
+          <v-card dark tile flat color="blue">
             <v-card-text>Reisetagebuch</v-card-text>
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
           </v-card>
         </v-flex>
 
         <v-flex xs6 ml-1>
-          <v-card dark tile flat color="red darken-4">
+          <v-card dark tile flat color="blue">
             <v-card-text>OBD</v-card-text>
+            <v-icon large>fas fa-tools</v-icon>
           </v-card>
         </v-flex>
 
-      </v-layout>
+    </v-layout> -->
 
+    <v-layout row>
+      <carousel>
+        <slide>
+          <v-flex mr-1 ml-1>
+          <v-card dark tile flat color="blue">
+            <v-card-text>Reisetagebuch</v-card-text>
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
+          </v-card>
+          </v-flex>
+        </slide>
+        <slide>
+          <v-flex mr-1 ml-1>
+          <v-card dark tile flat color="blue">
+            <v-card-text>Reisetagebuch</v-card-text>
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
+          </v-card>
+          </v-flex>
+        </slide>
+        <slide>
+          <v-flex mr-1 ml-1>
+          <v-card dark tile flat color="blue">
+            <v-card-text>Reisetagebuch</v-card-text>
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
+          </v-card>
+          </v-flex>
+        </slide>
+      </carousel>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+
+import { Carousel, Slide } from 'vue-carousel';
+
   export default {
+    components: {
+      Carousel,
+      Slide
+    },
     data: () => ({
       lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
       date: new Date().toISOString().substr(0, 10), 
@@ -63,6 +100,10 @@
   .d-flex {
     height: 10rem;
   }
+
+.VueCarousel {
+    width: 100%;
+}
 
 </style>
 
