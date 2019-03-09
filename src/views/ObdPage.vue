a50d51dc25a0ca683a35ea162d174a501420d9d1
Robin <robin.schwab@hf-furtwangen.de>
2019-03-09 11:54:56 +0100

    obd notification testing

--- src/views/ObdPage.vue
+++ src/views/ObdPage.vue
@@ -30,25 +30,38 @@ export default {
                 console.log(service);
                 return service.getCharacteristic(charUUID);
             })
+            .then(characteristic => characteristic.startNotifications())
             .then(characteristic => {
+                characteristic.addEventListener('characteristicvaluechanged',
+                this.handleCharacteristicValueChanged);
+
                 console.log(characteristic);
                 let encoder = new TextEncoder('utf-8');
-                let message = encoder.encode("ATZ\r\n");
+                // let message = encoder.encode("ATZ\r\n");
                 var data = new Uint8Array([0x01, 0x00, 0x0D]);
+                let message2 = encoder.encode("0105\r");
 
                 // var x = '\x01\x00\r'
                 // const buffer = new ArrayBuffer(3);
-                // const uint8 = new Uint8Array(buffer);
+                // const uint8 ATZ= new Uint8Array(buffer);
                 // uint8.set([01, 00, 13]);
 
-                characteristic.writeValue(message);
-                return characteristic.readValue();
+                // characteristic.writeValue(message);
+                return characteristic.writeValue(message2);
             })
             .then(value => {
                 console.log(value);
             })
             .catch(error => { console.log(error.message); });
+        },
+
+        handleCharacteristicValueChanged: function(event) {
+            var value = event.target.value;
+            console.log('Received ' + value);
+            // TODO: Parse Heart Rate Measurement value.
+            // See https://github.com/WebBluetoothCG/demos/blob/gh-pages/heart-rate-sensor/heartRateSensor.js
         }
+
     
     },
     mounted() {
