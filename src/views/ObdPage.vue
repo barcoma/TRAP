73d12400d477a5b2e2776ddc7ff7ab182f95e6ad
Robin <robin.schwab@hf-furtwangen.de>
2019-03-10 16:03:11 +0100

    clean workspace

--- src/views/ObdPage.vue
+++ src/views/ObdPage.vue
@@ -32,36 +32,57 @@ export default {
             })
             .then(characteristic => characteristic.startNotifications())
             .then(characteristic => {
+                let encoder = new TextEncoder('utf-8');
+
+                var commands = [];
+                commands.push(encoder.encode("ATD\r\n"))
+                commands.push(encoder.encode("ATZ\r\n"))
+                commands.push(encoder.encode("ATE0\r\n"))
+                commands.push(encoder.encode("ATL0\r\n"))
+                commands.push(encoder.encode("ATS0\r\n"))
+                commands.push(encoder.encode("ATH0\r\n"))
+                commands.push(encoder.encode("ATSP0\r\n"))
+
+                var i = 0;
+                while(i < commands.length)
+                {
+                    characteristic.writeValue(commands[i]);
+                    i++;
+                    this.sleep(1000);
+                }
                 characteristic.addEventListener('characteristicvaluechanged',
                 this.handleCharacteristicValueChanged);
 
                 console.log(characteristic);
-                let encoder = new TextEncoder('utf-8');
-                // let message = encoder.encode("ATZ\r\n");
-                var data = new Uint8Array([0x01, 0x00, 0x0D]);
-                let message2 = encoder.encode("0105\r");
-
+                var data = new Uint8Array([0x02, 0x01, 0x0C, 0x55, 0x55, 0x55, 0x55, 0x55, 0x0D]);
+                let message = encoder.encode("010C\r");
+                let message2 = encoder.encode("ATDP\r\n");
                 // var x = '\x01\x00\r'
                 // const buffer = new ArrayBuffer(3);
                 // const uint8 ATZ= new Uint8Array(buffer);
                 // uint8.set([01, 00, 13]);
 
-                // characteristic.writeValue(message);
-                return characteristic.writeValue(message2);
+                //characteristic.writeValue(message2);
+                return characteristic.writeValue(data);
             })
             .then(value => {
                 console.log(value);
             })
-            .catch(error => { console.log(error.message); });
+            .catch(error => { 
+                console.log(error.message); 
+            });
         },
 
         handleCharacteristicValueChanged: function(event) {
-            var value = event.target.value;
-            console.log('Received ' + value);
+            let test = event.target.value;
+            console.log('Received ' + test);
             // TODO: Parse Heart Rate Measurement value.
             // See https://github.com/WebBluetoothCG/demos/blob/gh-pages/heart-rate-sensor/heartRateSensor.js
+        },
+        sleep: function(delay) {
+            var start = new Date().getTime();
+            while (new Date().getTime() < start + delay);
         }
-
     
     },
     mounted() {
