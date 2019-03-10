88eb9a3b23518348858a5b37d5de1bb11484416d
Robin <robin.schwab@hf-furtwangen.de>
2019-03-10 16:05:50 +0100

    graphql tests WIP

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const request = require('request');
+
 
 const PORT = 4000;
 
@@ -11,11 +13,39 @@ const typeDefs = gql`
   }
 `;
 
+var test = yelpRequest();
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => "Hello World"
   },
 };
+
+
+function yelpRequest() {
+  var myJSONObject = `{
+    business(id: "garaje-san-francisco") {
+        name
+        id
+        rating
+        url
+    }
+  }`
+  
+  request({
+    url: "https://api.yelp.com/v3/graphql",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/graphql",
+      "Authorization": "Bearer FfLMJXvs0iVD5I5K8x5BHaz7dWbv64WzwSDOw0I_HkqNlc4aTaoUvd2KyFPAMi_Mhrqi-ABmzJbXhh-Tu44eqmeAuNwEZ6uJtDf5IyEaNlQkT_-dLnu5bwZK3_aAXHYx"
+    },
+    body: myJSONObject
+  }, function (error, response, body){
+    // console.log(body);
+    return body;
+  });
+  
+}
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
