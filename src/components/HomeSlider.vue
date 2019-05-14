2f7377765f3e05e8e67d9a7e2b334a1d3fe49f55
Henri Joß <henricharles@web.de>
2019-05-14 12:18:49 +0200

    structural changes

---
+++ src/components/HomeSlider.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+
+</template>
+ 
+<script lang="js">
+  export default  {
+    name: 'HomeSlider',
+    props: [],
+    mounted() {
+      
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+    
+    },
+    computed: {
+ 
+    }
+}
+</script>
+ 
+<style scoped lang="scss">
+
+</style>
