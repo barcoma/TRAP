fca2109b0c23c9eafc946d152fb6aa901f3bd2d7
Robin <robin.schwab@hf-furtwangen.de>
2019-05-17 11:56:50 +0200

    frontend custom poi implementation

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,11 +1,9 @@
-const MAPBOX_API_KEY = "pk.eyJ1IjoiYmFyY29tYSIsImEiOiJjam9xM3gwYWYwMHlpM3ZrZmY4NWNwam9kIn0.TE3Zma1nEd5mbbdVCfQGMA" ;
-const MapBoxDomain     = "mapbox.com/";
+importScripts("https://unpkg.com/dexie@2.0.3/dist/dexie.js");
 
 workbox.setConfig({
     debug: false,
   });
   
-  // workbox.precaching.precacheAndRoute([]);
   
   workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|svg)$/,
@@ -31,32 +29,44 @@ workbox.setConfig({
     workbox.strategies.cacheFirst(),
   ); 
     
-  workbox.routing.registerRoute(
-    new RegExp('mapbox'),
-    workbox.strategies.networkFirst({
-      cacheName: 'mapbox',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 50,
-        }),
-      ],
-    }),
-  );
+  // workbox.routing.registerRoute(
+  //   workbox.strategies.networkFirst({
+  //     cacheName: 'mapbox',
+  //     plugins: [
+  //       new workbox.expiration.Plugin({
+  //         maxAgeSeconds: 30 // * 24 * 60 * 60,
+  //       }),
+  //     ],
+  //   }),
+  // );
 
 self.addEventListener('fetch', function(event) {
-    var url = event.request.url;
+  var url = event.request.url;
 
-    if(url.startsWith('https://') && (url.includes('tiles.mapbox.com') || url.includes('api.mapbox.com'))) {
-      event.respondWith(
-        caches.match(event.request).then(function(resp) {
-          return resp || fetch(event.request).then(function(response) {
-            var cacheResponse = response.clone();
-            caches.open('mapbox').then(function(cache) {
-              cache.put(event.request, cacheResponse);
-            });
-            return response;
+  if (url.startsWith('https://') && (url.includes('tiles.mapbox.com') || url.includes('api.mapbox.com'))) {
+    event.respondWith(
+      caches.match(event.request).then(function(resp) {
+        return resp || fetch(event.request).then(function(response) {
+          var cacheResponse = response.clone();
+          caches.open('mapbox').then(function(cache) {
+            cache.put(event.request, cacheResponse);
           });
-        })
-      );
-    }  
+          return response;
+        });
+      })
+    );
+  }  
+
+  if (url.includes('localhost:4000/graphql')) {
+    event.respondWith(
+      // cache.match(event.request).then(function (response) {
+      //   return response || fetch(event.request).then(function(response) {
+      //     caches.open('local-graphql').then(function(cache) {
+      //       cache.put(event.request, response.clone());
+      //     });
+      //     return response;
+      //   });
+      // })
+    )
+  }
 })
