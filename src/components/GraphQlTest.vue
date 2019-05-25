171a7b46b9395e972c1edb71abd83709b90d5aed
Robin-Sb <robin.schwab@hs-furtwangen.de>
2019-05-25 17:25:15 +0200

    updated graphql test file

--- src/components/GraphQlTest.vue
+++ src/components/GraphQlTest.vue
@@ -1,90 +1,68 @@
 <template>
-<!-- <ApolloQuery
-  :query="queries.query"
-  :variables="{ term: 'food', latitude: 52.520008, longitude: 13.404954, radius: 50, limit: 15 }">
-  <template slot-scope="{ result: { loading, error, data } }">
-    {{data}}
-    <div v-if="loading" class="loading apollo">Loading...</div>
-
-    <div v-else-if="error" class="error apollo">An error occured</div>
-
-    <div v-else-if="data" class="result apollo"></div>
-
-    <div v-else class="no-result apollo">No result :(</div>
-  </template>
-  <button v-on:click="test">log console</button>
-</ApolloQuery> -->
 <div>
-    <button v-on:click="testIntercept">log console</button>
-    <div>{{query}}</div>
-</div>
+    <button v-on:click="testCache">log console</button>
+    </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
 
 export default {
-  // data () {
-  //   return {
-  //     queries: {
-  //       query: gql`
-  //       {
-  //         search 
-  //         {
-  //           total,
-  //           business {
-  //             name
-  //             coordinates {
-  //               latitude
-  //               longitude
-  //             }
-  //           }
-  //         }
-  //       }`
-  //     },
-  //     data: Object
-  //   }
-  // },
-// const yelpQuery = 
-// gql`
-//   {
-//     search 
-//     {
-//       total,
-//       business {
-//         name
-//         coordinates {
-//           latitude
-//           longitude
-//         }
-//       }
-//     }
-//   }`
 
   mounted() {
-    this.$apollo.queries.query.skip = true;
+    this.$apollo.queries.yelpPOI.skip = true;
   },
   apollo: {
-    query: gql`{
-      search
-      {
-        total
-        business {
-          name
-          coordinates {
-            latitude
-            longitude
-          }
+    yelpPOI: gql`{
+        yelpPOI
+        {
+            name
+            id
+            url
+            coordinates {
+                latitude
+                longitude
+            }
         }
-      }
     }`
   },
   methods: {
     testIntercept: function() {
-      this.$apollo.queries.query.skip = false;
+      this.$apollo.queries.yelpPOI.skip = false;
       var x = this.$apollo.queries.query.refetch({ term: 'food', latitude: 52.520008, longitude: 13.404954, radius: 50, limit: 15 })
       x.then(xd => console.log(xd.data.search));
     },
+    testCache: function() {
+        console.log(this.$apollo)
+        const query = this.$apollo.queries.yelpPOI;
+
+        const term = this.$apollo.provider.defaultClient.readQuery({
+            query: gql`
+            query yelpPOI {
+                yelpPOI {
+                    name
+                    id
+                    url
+                    coordinates {
+                        latitude
+                        longitude
+                    }
+                }
+            }`
+            });
+            console.log(term);
+        // const test = this.$apollo.provider.defaultClient.readFragment({
+        //     id: "YMHJ8K1sdwVCw4XO8ttDNA",
+        //     fragment: gql`
+        //     fragment poi on yelpPOI {
+        //         name
+        //         id
+        //         url
+        //     }
+        // `
+        // });
+        // console.log(test);
+    }
   }
 }
 </script>
