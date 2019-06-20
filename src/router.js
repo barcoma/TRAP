4456aeafb008e6d6c2e314c67519bf5763273417
janschu1 <jan.schultis@web.de>
2019-06-20 13:14:54 +0200

    ...

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 import ObdPage from './views/ObdPage.vue'
 import GraphQl from './components/GraphQlTest.vue'
 import POIPage from './views/POIPage.vue'
+import lastdest from './views/lastdest.vue'
 
 Vue.use(Router)
 
@@ -34,6 +35,11 @@ export default new Router({
       path: '/poi',
       name: 'poi',
       component: POIPage
+    },
+    {
+      path: '/lastdest',
+      name: 'lastdest',
+      component: lastdest
     }
   ]
 })
