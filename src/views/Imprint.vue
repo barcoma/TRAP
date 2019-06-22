3c9c857487d6f864713dfd1b7e66850d2db8d7df
Henri Joß <henricharles@web.de>
2019-06-22 10:35:27 +0200

    imprint & privacy pages

---
+++ src/views/Imprint.vue
@@ -0,0 +1,26 @@
+<template>
+</template>
+
+
+<script>
+import { eventBus } from '../main.js'
+
+export default {
+  name: 'Imprint',
+  components: {
+
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
