6cebc1bfb27ca7820ca4985a442616d0036e009d
Henri Joß <henricharles@web.de>
2019-06-28 14:37:08 +0200

    last destinations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,153 +48,6 @@
 
     </v-layout>
 
-    <v-layout row>
-      <div>
-        <li v-for="destination in lastDestination" v-bind:key="destination.id">
-          {{destination.name}} 
-        </li>
-
-      </div>
-
-      <div>
-        <v-tabs
-          v-model="active"
-          color="white"
-          light
-          centered
-          grow
-          active-class="active-tab"
-        >
-          <v-tab href="#tab-1">
-            Letzte Ziele
-          </v-tab>
-          <v-tab href="#tab-2">
-            Favoriten
-          </v-tab>
-          <v-tab href="#tab-3">
-            Entdecken
-          </v-tab>
-          <v-tab-item
-                v-for="i in 3"
-                :key="i"
-                :value="'tab-' + i"
-                lazy
-          >
-
-            <carousel v-if="i == 1"
-            :paginationEnabled="false"
-            >
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://images.unsplash.com/photo-1436637706755-81d219b36e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Eifelturm</div>
-                        <div class="carousel-tex-subheader">Paris, France</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://images.unsplash.com/photo-1486247496048-cc4ed929f7cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Louvre</div>
-                        <div class="carousel-tex-subheader">Paris, France</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://picsum.photos/510/300?random"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Martin</div>
-                        <div class="carousel-tex-subheader">Paris, France</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-            </carousel>
-
-
-
-
-            <carousel v-if="i == 2"
-            :paginationEnabled="false"
-            >
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://www.schwarzwald-geniessen.de/eip/clips/lightbox_eingang.jpg?fl=18139758"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Uhrenmuseum</div>
-                        <div class="carousel-tex-subheader">Furtwangen, Germany</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://www.schwarzwald-geniessen.de/eip/clips/lightbox_eingang.jpg?fl=18139758"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Kebab-Treff</div>
-                        <div class="carousel-tex-subheader">Furtwangen, Germany</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-              <slide>
-                <v-flex mr-1 ml-1>
-                  <v-card dark tile flat
-                  img="https://picsum.photos/510/300?random"
-                  height="30vh"
-                  >        
-                    <v-card-title>
-                      <v-icon medium>location_on</v-icon>
-                      <div class="carousel-text">
-                        <div class="carousel-tex-headline">Martin</div>
-                        <div class="carousel-tex-subheader">Paris, France</div>
-                      </div>
-                    </v-card-title>                
-                  </v-card>
-                </v-flex>
-              </slide>
-            </carousel>
-
-          </v-tab-item>
-        </v-tabs>
-      </div>
-    </v-layout>
-
     <div class="last-destinations-container">
       <div v-if="weather" class="weather-container">
         <h3 class="weather-location">{{ currentLocation }}</h3>
@@ -206,8 +59,8 @@
       </div>
       <h3 class="last-destinations">Letzte Ziele</h3>
       <ul class="last-locations-list">
-        <li v-for="item in lastLocations" :key="item.id">
-          <v-icon color="white">near_me</v-icon> {{ item.location }}
+        <li v-for="destination in lastDestination" :key="destination.id">
+          <v-icon color="white">near_me</v-icon> {{ destination.name }}
         </li>
       </ul>
       <hr class="spacer"/>
@@ -225,8 +78,7 @@ import axios from 'axios'
 import mapboxgl from 'mapbox-gl'
 import MapboxGeocoder from 'mapbox-gl-geocoder'
 import Sidebarmenu from '../components/Sidebarmenu.vue'
-import { weather } from '../shared_data/queries'
-import { getLastDestination } from '../shared_data/queries'
+import { weather, getLastDestination } from '../shared_data/queries'
 
   export default {
     components: {
@@ -252,13 +104,6 @@ import { getLastDestination } from '../shared_data/queries'
       temp: weather.temp,
       temp_max: weather.temp_max,
       temp_min: weather.temp_min,
-      lastLocations: [
-        { location: 'Furtwangen' },
-        { location: 'Stuttgart' },
-        { location: 'Freiburg' },
-        { location: 'Köln' }
-      ],
-      rain: '',
       lastDestination: [{
         "name": "Keine letzten Ziele gefunden!",
         id: 0
@@ -379,7 +224,6 @@ import { getLastDestination } from '../shared_data/queries'
 
   h1,h2,h3,h4 p {
     color: white;
-    font-family: 'Roboto Slab', serif;
   }
   h3{
     font-weight: 400;
@@ -397,9 +241,13 @@ import { getLastDestination } from '../shared_data/queries'
     margin-bottom: 1rem;
     border-bottom-left-radius: 7%;
     border-bottom-right-radius: 7%;
-    background: -moz-linear-gradient(-60deg, #4285f4 0%, #00ebff 100%); /* FF3.6-15 */
-    background: -webkit-linear-gradient(-60deg, #4285f4 0%,#00ebff 100%); /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(135deg, #4285f4 0%,#00ebff 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    // background: -moz-linear-gradient(-60deg, #4285f4 0%, #00ebff 100%); /* FF3.6-15 */
+    // background: -webkit-linear-gradient(-60deg, #4285f4 0%,#00ebff 100%); /* Chrome10-25,Safari5.1-6 */
+    // background: linear-gradient(135deg, #4285f4 0%,#00ebff 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    background: #00d2ff;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(#3a7bd5, #00d2ff);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(#3a7bd5, #00d2ff); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 
     .flex-avatar{
       img {
@@ -530,18 +378,26 @@ import { getLastDestination } from '../shared_data/queries'
 }
 
 hr.spacer {
-    height: 6rem;
+    height: 16rem;
     opacity: 0;
 }
 
 .last-destinations-container {
-    background: -moz-linear-gradient(-60deg, #4285f4 0%, #00ebff 100%); /* FF3.6-15 */
-    background: -webkit-linear-gradient(-60deg, #4285f4 0%,#00ebff 100%); /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(135deg, #4285f4 0%,#00ebff 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    // background: -moz-linear-gradient(-60deg, #4285f4 0%, #00ebff 100%); /* FF3.6-15 */
+    // background: -webkit-linear-gradient(-60deg, #4285f4 0%,#00ebff 100%); /* Chrome10-25,Safari5.1-6 */
+    // background: linear-gradient(135deg, #4285f4 0%,#00ebff 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    background: #00d2ff;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(#00d2ff, #8E54E9);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(#00d2ff, #8E54E9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
     border-radisu: 17px;
     border-radius: 22px;
     margin-top: 1rem;
     padding-top: 1rem;
+}
+
+.app-nav .v-card {
+  border-radius: 0px !important;
 }
 
 </style>
